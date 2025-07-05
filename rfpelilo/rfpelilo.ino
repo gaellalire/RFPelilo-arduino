ac54fac3933a398c5a6edf851b0db0d0226677ed
Gaël Lalire <gael.lalire@gaellalire.fr>
2025-07-05 10:31:10 +0200

    Support of ARDUINO_ADAFRUIT_FEATHER_RP2040_RFM

--- rfpelilo/rfpelilo.ino
+++ rfpelilo/rfpelilo.ino
@@ -1,6 +1,7 @@
 // #define RF69_RADIO 1
-#define CC1101_RADIO 1
-#define BLE_ACTIVATED 1
+// #define CC1101_RADIO 1
+// #define BLE_ACTIVATED 1
+// #define RF69_HIGH_POWER 1
 
 #define TIMEOUT_MILLI 500
 #define BLE_CHARACTERISTIC_SIZE 230
@@ -22,15 +23,19 @@
 // RST pin:   unused
 // GDO2 pin:  3
 CC1101 radio = new Module(10, 2, RADIOLIB_NC, 3);
+#define MAX_PACKET_LENGTH RADIOLIB_CC1101_MAX_PACKET_LENGTH
 #elif defined(RF69_RADIO)
 // RF69 has the following connections:
 // CS pin:    10
-// DIO0 pin:  2
-// RESET pin: 3
-RF69 radio = new Module(10, 2, 3);
+// DIO0 pin:  3
+// RESET pin: 2
+// DIO1 pin:  4
+RF69 radio = new Module(10, 3, 2, 4);
+#define MAX_PACKET_LENGTH RADIOLIB_RF69_MAX_PACKET_LENGTH
 #elif defined(ARDUINO_ADAFRUIT_FEATHER_RP2040_RFM)
 // use internal radio pin
-RF69 radio = new Module(16, 21, 17);
+RF69 radio = new Module(16, 21, 17, 22);
+#define MAX_PACKET_LENGTH RADIOLIB_RF69_MAX_PACKET_LENGTH
 #endif
 
 ::rfpelilo::Main request_;
@@ -38,6 +43,16 @@ RF69 radio = new Module(16, 21, 17);
 
 EmbeddedProto::ReadBufferFixedSize<550> read_buffer_;
 EmbeddedProto::WriteBufferFixedSize<550> write_buffer_;
+
+volatile bool fifoEmpty = false;
+
+#if defined(ESP8266) || defined(ESP32)
+ICACHE_RAM_ATTR
+#endif
+void fifoAdd(void) {
+  // we can send more bytes
+  fifoEmpty = true;
+}
 
 #if defined(BLE_ACTIVATED)
 
@@ -66,7 +81,7 @@ void txCharacteristicWritten(BLEDevice central, BLECharacteristic characteristic
   read_buffer_.set_bytes_written(valueLength);
   byte b = 0;
   // first byte is size
-  read_buffer_.pop(b);  
+  read_buffer_.pop(b);
   if (b & 0x80) {
     read_buffer_.pop(b);  // first byte is also size, no third because MTU < 230 < 255
   }
@@ -93,8 +108,6 @@ void txCharacteristicWritten(BLEDevice central, BLECharacteristic characteristic
 
 #endif
 
-
-int16_t transmitError;
 
 void setup() {
   Serial.begin(115200);
@@ -123,6 +136,13 @@ void setup() {
       delay(500);
     }
   }
+
+#if defined(RF69_RADIO) || defined(ARDUINO_ADAFRUIT_FEATHER_RP2040_RFM)
+  radio.setFifoEmptyAction(fifoAdd);
+#if defined(RF69_HIGH_POWER) || defined(ARDUINO_ADAFRUIT_FEATHER_RP2040_RFM)
+  radio.setOutputPower(0, true);
+#endif
+#endif
 
 #if defined(BLE_ACTIVATED)
 
@@ -155,11 +175,20 @@ void setup() {
 
 void sendRF(rfpelilo::SendRFRequest sendRFRequest) {
   response_.set_command_id(request_.get_command_id());
+  uint32_t messageLength = sendRFRequest.get_data().get_length();
   //  radio.setModulation(Modulation. sendRFRequest.get_modulation());  // set modulation mode. 0 = 2-FSK, 1 = GFSK, 2 = ASK/OOK, 3 = 4-FSK, 4 = MSK.
-  if (radio.fixedPacketLengthMode(sendRFRequest.get_data().get_length()) != RADIOLIB_ERR_NONE) {
-    response_.set_command_status(rfpelilo::CommandStatus::RF_PACKET_LENGTH_ERROR);
-    return;
+  if (messageLength > MAX_PACKET_LENGTH) {
+    if (radio.fixedPacketLengthMode(0) != RADIOLIB_ERR_NONE) {
+      response_.set_command_status(rfpelilo::CommandStatus::RF_PACKET_LENGTH_ERROR);
+      return;
+    }
+  } else {
+    if (radio.fixedPacketLengthMode(messageLength) != RADIOLIB_ERR_NONE) {
+      response_.set_command_status(rfpelilo::CommandStatus::RF_PACKET_LENGTH_ERROR);
+      return;
+    }
   }
+
   if (radio.setFrequency(sendRFRequest.get_frequency()) != RADIOLIB_ERR_NONE) {
     response_.set_command_status(rfpelilo::CommandStatus::RF_FREQUENCY_ERROR);
     return;
@@ -170,7 +199,8 @@ void sendRF(rfpelilo::SendRFRequest sendRFRequest) {
       return;
     }
   }
-  if (radio.setBitRate(sendRFRequest.get_dataRate()) != RADIOLIB_ERR_NONE) {
+  float bitRate = sendRFRequest.get_dataRate();
+  if (radio.setBitRate(bitRate) != RADIOLIB_ERR_NONE) {
     response_.set_command_status(rfpelilo::CommandStatus::RF_DATA_RATE_ERROR);
     return;
   }
@@ -179,14 +209,58 @@ void sendRF(rfpelilo::SendRFRequest sendRFRequest) {
     repeat = sendRFRequest.get_repeat();
   }
 
+  int16_t transmitError = RADIOLIB_ERR_NONE;
   rfpelilo::CommandStatus transmitStatus = rfpelilo::CommandStatus::RF_TRANSMIT_FAILED;
+  uint8_t *txBuffPtr = (uint8_t *)sendRFRequest.get_data().get_const();
   for (int i = 0; i < repeat; i++) {
-    transmitError = radio.transmit((uint8_t *)sendRFRequest.get_data().get_const(), sendRFRequest.get_data().get_length());
-    if (RADIOLIB_ERR_NONE == transmitError) {
-      transmitStatus = rfpelilo::CommandStatus::OK;
+    if (messageLength > MAX_PACKET_LENGTH) {
+#if defined(RF69_RADIO) || defined(ARDUINO_ADAFRUIT_FEATHER_RP2040_RFM)
+      int remLength = messageLength;
+      // calculate timeout (5ms + 500 % of expected time-on-air)
+      RadioLibTime_t timeout = 5 + (RadioLibTime_t)((((float)(messageLength * 8)) / bitRate) * 5);
+      RadioLibTime_t start = millis();
+
+      transmitError = radio.startTransmit(txBuffPtr, messageLength);
+
+      bool transmittedFlag = false;
+      bool timeoutExpired = false;
+      while (!transmittedFlag) {
+        if (millis() - start > timeout) {
+          // timeout
+          timeoutExpired = true;
+          break;
+        }
+        if (fifoEmpty) {
+          // reset flag
+          fifoEmpty = false;
+
+          // add more bytes to the transmit buffer
+          transmittedFlag = radio.fifoAdd(txBuffPtr, messageLength, &remLength);
+        }
+      }
+      radio.standby();
+      if (!timeoutExpired) {
+        transmitStatus = rfpelilo::CommandStatus::OK;
+      }
+#endif
+    } else {
+      transmitError = radio.transmit((uint8_t *)sendRFRequest.get_data().get_const(), messageLength);
+      if (RADIOLIB_ERR_NONE == transmitError) {
+        transmitStatus = rfpelilo::CommandStatus::OK;
+      }
     }
   }
+
   response_.set_command_status(transmitStatus);
+
+  if (transmitError != RADIOLIB_ERR_NONE) {
+    EmbeddedProto::FieldString<200> textValue;
+    String s = "TransmitError " + String(transmitError);
+    textValue.set(s.c_str());
+    rfpelilo::ErrorMessage errorMessage;
+    errorMessage.set_text(textValue);
+    response_.set_error_message(errorMessage);
+  }
 }
 
 void sendError(String errorText) {
@@ -212,17 +286,15 @@ void loop() {
   read_buffer_.clear();
 
   while (!Serial.available()) {
+#if defined(BLE_ACTIVATED)
     BLE.poll();
+#endif
   }
-  uint32_t originalMessageSize = 0;
   uint32_t messageSize = 0;
   // read 2 bytes
-  int8_t byte1 = Serial.read();
-  read_buffer_.push(byte1);
+  uint8_t byte = Serial.read();
+  read_buffer_.push(byte);
   last = millis();
-  int8_t byte2 = 0;
-  int8_t byte = 0, byten1 = 0, bytem1 = 0, bytem2 = 0, bytem3 = 0;
-  uint8_t fbyte = 0;
 
   do {
     while (!Serial.available()) {
@@ -233,22 +305,18 @@ void loop() {
       }
       last = millis();
     }
-    byte2 = Serial.read();
-    read_buffer_.push(byte2);
+    byte = Serial.read();
+    read_buffer_.push(byte);
     if (EmbeddedProto::Error::NO_ERRORS != ::EmbeddedProto::WireFormatter::DeserializeVarint(read_buffer_, messageSize)) {
       // error : clear input
       while (Serial.available()) { Serial.read(); }
       goto input_error;
     }
-    // read_buffer_.pop(byte);
-    originalMessageSize = messageSize;
-    // read_buffer_.push(byte2);
-    if (read_buffer_.peek(fbyte)) {
+    if (read_buffer_.peek(byte)) {
       // not empty : 1 byte size
       messageSize--;
       // we already read one byte from the message
     }
-    int bytem1set = 0;
     while (messageSize > 0) {
       while (!Serial.available()) {
         if (millis() - last > TIMEOUT_MILLI) {
@@ -258,23 +326,10 @@ void loop() {
         }
         last = millis();
       }
-      byten1 = byte;
 
       byte = Serial.read();
 
-      if (bytem1set == 0) {
-        bytem1 = byte;
-        bytem1set++;
-      } else if (bytem1set == 1) {
-        bytem2 = byte;
-        bytem1set++;
-
-      } else if (bytem1set == 2) {
-        bytem3 = byte;
-        bytem1set++;
-      }
       read_buffer_.push(byte);
-      //  originalMessageSize = byte;
 
       messageSize--;
     }
@@ -286,20 +341,11 @@ void loop() {
       sendRF(sendRFRequest);
       digitalWrite(LED_BUILTIN, LOW);
 
-
-      EmbeddedProto::FieldString<200> textValue;
-      String s = String(byte1) + " DES_OK " + String(byte2) + " or " + String(originalMessageSize) + " length " + sendRFRequest.get_data().get_length() + " RADIOLIB_CC1101_MAX_PACKET_LENGTH " + String(RADIOLIB_CC1101_MAX_PACKET_LENGTH) + " transmitError " + String(transmitError);
-      textValue.set(s.c_str());
-      rfpelilo::ErrorMessage errorMessage;
-      errorMessage.set_text(textValue);
-      response_.set_error_message(errorMessage);
-
-
       ::EmbeddedProto::WireFormatter::SerializeVarint(response_.serialized_size(), write_buffer_);
       if (EmbeddedProto::Error::NO_ERRORS == response_.serialize(write_buffer_)) {
         Serial.write(write_buffer_.get_data(), write_buffer_.get_size());
       } else {
-        goto input_error;
+        sendError("Cannot serialize");
       }
     } else {
       goto input_error;
@@ -308,6 +354,5 @@ void loop() {
   } while (false);
 
 input_error:
-  String errorText = String("input_error b1 ") + String(byte1) + " b2 " + String(byte2) + " or " + String(originalMessageSize) + " messageSize " + String(messageSize) + " fbyte " + String((int8_t)fbyte) + " ms " + String(messageSize) + " bytem1 " + String(bytem1) + " bytem2 " + String(bytem2) + " bytem3 " + String(bytem3) + " ... byten1 " + String(byten1) + " byten " + String(byte);
-  sendError(errorText);
+  sendError("Cannot deserialize");
 }
